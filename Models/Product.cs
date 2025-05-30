4ed19c4ef9b14fe8f9fe2d661425a2008c50ccb1
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-05-30 20:02:13 +0500

    feat: добавлены новые свойства в класс Product
    - Добавлено свойство `IkpuCode` (строка, допускающая `null`).
    - Добавлено свойство `UnitCode` (строка, допускающая `null`).

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,5 +5,11 @@
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        
+
+
+
+        public string? IkpuCode { get; set; }
+        public string? UnitCode { get; set; }
     }
 }
