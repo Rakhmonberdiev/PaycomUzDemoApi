55416bb9b3dced19f0d3a71eb58330c4a0d0d869
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-05-30 19:58:15 +0500

    feat: добавлен класс Product для модели продукта
    - Добавлен новый файл `Product.cs` с определением класса `Product`.
    - Класс `Product` включен в пространство имен `PaycomUzDemoApi.Models`.
    - Свойства класса:
      - `Id` (`Guid`) для идентификации продукта.
      - `Name` (`string`) с начальным значением `string.Empty` для названия.
      - `Price` (`decimal`) для хранения цены продукта.

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace PaycomUzDemoApi.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
