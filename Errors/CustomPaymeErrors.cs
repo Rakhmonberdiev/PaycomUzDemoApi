034eece2e3c8ac7ca7bb9ec625cea90ac4e959d8
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-05-30 20:28:54 +0500

    feat: добавлен класс CustomPaymeErrors
    - Создан новый файл `CustomPaymeErrors.cs` с одноимённым статическим
      классом в пространстве имен `PaycomUzDemoApi.Errors`.
    - Добавлены директивы `using` для подключения пространств имен:
      `PaycomUz.Models.Common` и `PaycomUz.Models.Errors`.
    - В класс `CustomPaymeErrors` добавлены два статических свойства:
      - `UserNotFound` с кодом ошибки `-31050` и локализованными
        сообщениями на узбекском, русском и английском языках.
      - `OrderNotFound` с кодом ошибки `-31050` и локализованными
        сообщениями на узбекском, русском и английском языках.

---
+++ Errors/CustomPaymeErrors.cs
@@ -0,0 +1,31 @@
+﻿using PaycomUz.Models.Common;
+using PaycomUz.Models.Errors;
+
+namespace PaycomUzDemoApi.Errors
+{
+    public static class CustomPaymeErrors
+    {
+        public static PaymeError UserNotFound => new PaymeError
+        {
+            Name = "UserNotFound",
+            Code = -31050,
+            Message = new LocalizedMessage
+            {
+                Uz = "Foydalanuvchi topilmadi",
+                Ru = "Пользователь не найден",
+                En = "User not found"
+            }
+        };
+        public static PaymeError OrderNotFound => new PaymeError
+        {
+            Name = "OrderNotFound",
+            Code = -31050,
+            Message = new LocalizedMessage
+            {
+                Uz = "Buyurtma topilmadi",
+                Ru = "Заказ не найден",
+                En = "Order not found"
+            }
+        };
+    }
+}
